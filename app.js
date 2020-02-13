211778d67ada4ad77af3d98855e9e3496fa773c3
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-13 01:07:45 +0100

    basic express.js and routes

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another  the middleware');
+     res.send('<h1> Add product page!</h1>')
+});
+app.use('/', (req, res, next) => {
+    console.log('In another  the middleware');
+     res.send('<h1> Hello from express!</h1>');
+});
 
-const server = http.createServer(routes.handler);
-
-server.listen(3000);
+app.listen(3000);
