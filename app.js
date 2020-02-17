bcc537dcce1e9af4b89d209a9ae107b8988401ec
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-17 18:04:33 +0100

    Filtering Paths

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 
 app.use(shopRoutes);
-app.use(adminRoutes);
+app.use('/admin',adminRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1> Pge not found</h1>');
+    res.status(404).send('<h1> Page not found</h1>');
 });
 
 app.listen(3000);
