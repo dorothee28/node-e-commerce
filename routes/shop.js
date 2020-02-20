c7f7c881cb93ac169ca390074c8e27905fc20b92
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-21 00:06:34 +0100

    Converting our Project to Handlebars

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const adminData = require('./admin')
 
 router.get('/', (req, res, next) => { 
     const products = adminData.products; 
-    res.render('shop', {prods: products, pageTitle: 'shop', path: '/shop'});
+    res.render('shop', {prods: products, pageTitle: 'shop', path: '/', hasProducts: products.length > 0});
 });
 
 module.exports = router;
